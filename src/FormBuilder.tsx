2137d1204eea2ce44a1e561e9593dfb05a0ed35a
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2024-03-07 14:50:36 +0100

    add form builder

---
+++ src/FormBuilder.tsx
@@ -0,0 +1,64 @@
+import { useRef } from "react";
+import "./css/form.css"
+import { Button } from "@nextui-org/react";
+
+type FormObject = {
+    [key: string]: any
+}
+
+export default class FormBuilder {
+    formName: string = "";
+    dataNames: Array<string> = [];
+
+    constructor(name: string) {
+        this.formName = name;
+    }
+
+    addDataNames(...rest: Array<string>) {
+        this.dataNames.push(...rest);
+    }
+
+    render(showForm: boolean | null): JSX.Element {
+        const myRefs = useRef<HTMLInputElement[]>([]);
+
+        const addToRefs = (element: HTMLInputElement) => {
+            if (element && !myRefs.current.includes(element)) {
+                myRefs.current.push(element);
+            }
+        };
+
+        const submit = () => {
+            const formObject: FormObject = {};
+    
+            this.dataNames.forEach((name, index) => {
+                formObject[name] = myRefs.current[index].value;
+            })
+    
+            console.log(formObject);
+        }
+
+        return (
+            <>
+                <form className="formBuilder" method="POST" style={{display: showForm ? 'block' : 'none'}}>
+                    <div style={{textAlign: 'center', paddingBottom: '1rem'}}>
+                        {this.formName}
+                    </div>
+                    {this.dataNames.map(dataName => {
+                        return (
+                            <div key={dataName} className="formElementDiv">
+                                <label htmlFor={dataName}>{dataName[0].toUpperCase() + dataName.slice(1)}</label>
+                                <input ref={addToRefs} type="text" id={dataName} name={dataName} required />
+                            </div>
+                        )
+                    })}
+                    <div className="flex justify-center">
+                        <Button type="submit" className="shad-button_primary px-8 py-1 rounded-md mt-4 w-30 place-self-center" onClick={submit}>
+                            Submit
+                        </Button>
+                    </div>
+                    {/* <button type="submit" onClick={this.submit}></button> */}
+                </form>
+            </>
+        )
+    }
+}
--- src/_root/pages/ManageCourse.tsx
+++ src/_root/pages/ManageCourse.tsx
@@ -1,15 +1,24 @@
+import FormBuilder from "@/FormBuilder";
 import { TableBuilder } from "@/TableBuilder";
 // import UserTables from "@/components/ui/TableComponent/UserTables"
 import { Button } from "@/components/ui/button"
 // import { Input } from "@/components/ui/input"
 import useFetch from "@/hooks/useFetch";
+import { useState } from "react";
 
 const table = new TableBuilder();
 
 table.addHeaders("code", "name", "department", "classType");
 
+const form = new FormBuilder("Add Course");
+
+form.addDataNames("code", "name", "department", "classType", "instructors", "units");
+
+form.addDataNames("teachingHours", "outstandingHours", "semester", "level");
+
 const ManageCourses = () => {
     const response = useFetch('http://localhost:3000/courses');
+    const [showForm, setShowForm] = useState<boolean | null>(null);
 
     if(response.data) {
         response.data.forEach((data: any) => {
@@ -19,13 +28,14 @@ const ManageCourses = () => {
 
     return (
         <div>
+            {form.render(showForm)}
             <div className='flex flex-row'>
                 <h1 className='h2-bold py-4 px-16 text-purple-700' >Manage Courses</h1>
                 <p className=''>Admin</p>
             </div>
             <section style={{backgroundColor: 'white', paddingRight: '2rem'}}>
                 <div className='flex justify-end'>
-                    <Button type="submit" className="shad-button_primary mt-4 w-30 place-self-center">
+                    <Button type="submit" className="shad-button_primary mt-4 w-30 place-self-center" onClick={() => setShowForm(true)}>
                         <div>Add New Course</div>
                     </Button>
                 </div>

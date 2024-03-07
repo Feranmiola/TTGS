72c620e8d879013959453e3481de25c88a106d5b
Joshua Ubani-Wokoma <88754789+JuszJo@users.noreply.github.com>
2024-03-07 08:04:34 +0100

    Revert "Created TableBuilder to easily make tables in this project"

--- src/_root/pages/ManageCourse.tsx
+++ src/_root/pages/ManageCourse.tsx
@@ -1,39 +1,26 @@
-import { TableBuilder } from "@/TableBuilder";
-// import UserTables from "@/components/ui/TableComponent/UserTables"
 import { Button } from "@/components/ui/button"
-// import { Input } from "@/components/ui/input"
-import useFetch from "@/hooks/useFetch";
-
-const table = new TableBuilder();
-
-table.addHeaders("code", "name", "department", "classType");
+import { Input } from "@/components/ui/input"
 
 const ManageCourses = () => {
-    const response = useFetch('http://localhost:3000/courses');
+  return (
+    <div>
+      <div className='flex flex-row'>
+        <h1 className='h2-bold py-14 px-16 text-purple-700' >Manage Courses</h1>
+        <p className='place-self'>Admin</p>
+      </div>
+      
+      {/* <div className="flex w-screen  bg-white">
+        <Input type="text" placeholder="Search" className="w-21 border-purple-600" />
+        <Button className="shad-button_primary" type="submit"
+        >Add New Lecturer </Button>
+      </div> */}
+    
+      {/* <UserTables/> */}
 
-    if(response.data) {
-        response.data.forEach((data: any) => {
-            table.addRow(data); 
-        })
-    }
 
-    return (
-        <div>
-            <div className='flex flex-row'>
-                <h1 className='h2-bold py-4 px-16 text-purple-700' >Manage Courses</h1>
-                <p className=''>Admin</p>
-            </div>
-            <section style={{backgroundColor: 'white', paddingRight: '2rem'}}>
-                <div className='flex justify-end'>
-                    <Button type="submit" className="shad-button_primary mt-4 w-30 place-self-center">
-                        <div>Add New Course</div>
-                    </Button>
-                </div>
-                {response.data && table.render()}
-            </section>
-
-        </div>
-    )
+      
+    </div>
+  )
 }
 
 export default ManageCourses
